fea0c982c3aeae598383ec286d8cbad14762a6e0
Xiao Ling <lingxiao@seas.upenn.edu>
2018-04-15 17:45:22 -0400

    add

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -1,7 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const fs = require("fs");
 const index = express_1.Router();
+index.get('/curriculum-vitae', (req, res, next) => {
+    var pdf_path = "/Users/lingxiao/Documents/Career/Resume/CV/resume.pdf";
+    fs.readFile(pdf_path, (err, data) => {
+        res.contentType('application/pdf');
+        res.send(data);
+    });
+});
 index.get('/', (req, res, next) => {
     res.render('/Users/lingxiao/Documents/Career/lingxiaoling.me/public/site/index', {});
 });
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,8 +7,24 @@ import { Router }   from 'express';
 import * as express from 'express';
 import * as path    from 'path'   ;
 import * as pug     from 'pug'    ;
+import * as fs from 'fs';
+
+// fs = require('fs')
+
 
 const index: Router = Router();
+
+/**
+	serve CV
+*/
+index.get('/curriculum-vitae', (req, res, next) => { 
+	var pdf_path = "/Users/lingxiao/Documents/Career/Resume/CV/resume.pdf"
+	fs.readFile( pdf_path, (err, data) =>{
+		res.contentType('application/pdf');
+		res.send(data)
+	})
+});
+
 
 /**
 	Template under development
@@ -30,7 +46,6 @@ index.get('/alt', (req, res, next) => {
 index.get('/alt-1', (req, res, next) => { 
 	res.render('/Users/lingxiao/Documents/Career/lingxiaoling.me/public/site/alt-1.pug', {});
 });
-
 
 
 export default index;
