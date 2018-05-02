3ff3fe5016101d0d01bd3d66214024f95fa8e70b
Xiao Ling <lingxiao@seas.upenn.edu>
2018-05-02 18:00:11 -0400

    add

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -4,7 +4,7 @@ const express_1 = require("express");
 const fs = require("fs");
 const url = require("url");
 const index = express_1.Router();
-function render_pdf(path, res) {
+function serve_pdf(path, res) {
     var pdf_path = url.resolve(__dirname, path);
     fs.readFile(pdf_path, (err, data) => {
         res.contentType('application/pdf');
@@ -12,10 +12,10 @@ function render_pdf(path, res) {
     });
 }
 index.get('/curriculum-vitae', (req, res, next) => {
-    return render_pdf('../public/curriculum-vitae/resume.pdf', res);
+    return serve_pdf('../public/curriculum-vitae/resume.pdf', res);
 });
 index.get('/final_hj_ling', (req, res, next) => {
-    return render_pdf('../public/assets/pdf/final_hj_ling.pdf', res);
+    return serve_pdf('../public/assets/pdf/final_hj_ling.pdf', res);
 });
 index.get('/', (req, res, next) => {
     res.render('site/index-home', {});
